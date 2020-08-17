baae1827fc7d2988ae42c748f2c84f17c3c08a4c
Ruihan Wang <wang_ruihan@hotmail.com>
2020-08-17 16:02:22 +0800

    move to header-only

--- include/nfl/prng/fastrandombytes.h
+++ include/nfl/prng/fastrandombytes.h
@@ -7,8 +7,22 @@
 #ifndef FASTRANDOMBYTES_H
 #define FASTRANDOMBYTES_H
 
+#include <sodium.h>
+#include <iostream>
+
 namespace nfl {
-void fastrandombytes(unsigned char *r, unsigned long long rlen);
+
+void fastrandombytes(unsigned char *r, unsigned long long rlen) {
+    static int rc = sodium_init();
+    
+    if (rc < 0) {
+        std::cerr << "LibSodium initialization failed" << std::endl;
+        throw std::runtime_error("LibSodium can not be initialized");
+    }
+
+    randombytes_buf(reinterpret_cast<void*>(r), rlen);  
+}
+
 }
 
 #endif
--- include/nfl/prng/randombytes.h
+++ include/nfl/prng/randombytes.h
@@ -7,8 +7,22 @@ Public domain.
 #ifndef RANDOMBYTES_H
 #define RANDOMBYTES_H
 
+#include <sodium.h>
+#include <iostream>
+
 namespace nfl {
-void randombytes(unsigned char *, unsigned long long);
+
+void randombytes(unsigned char *r, unsigned long long rlen) {
+    static int rc = sodium_init();
+    
+    if (rc < 0) {
+        std::cerr << "LibSodium initialization failed" << std::endl;
+        throw std::runtime_error("LibSodium can not be initialized");
+    }
+
+    randombytes_buf(reinterpret_cast<void*>(r), rlen);
+}
+
 }
 
 #endif
